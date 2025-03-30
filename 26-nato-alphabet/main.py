7beaf1f38e6b8a7230f040c6a81ec80d57d9b672
jpsilvadev <jpsjoaosilva98@gmail.com>
2025-03-30 16:40:47 +0100

    day26

---
+++ 26-nato-alphabet/main.py
@@ -0,0 +1,21 @@
+"""Bravo Delta Sierra"""
+import pandas as pd
+
+df = pd.read_csv("nato_phonetic_alphabet.csv")
+
+
+code_dic = {row.letter: row.code for (index, row) in df.iterrows()}
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    letters = list(word)
+    try:
+        result = [code_dic[letter] for letter in letters]
+    except KeyError:
+        print("Sorry, only letters in the alphabet.")
+        generate_phonetic()
+    else:
+        print(result)
+        
+        
+generate_phonetic()
